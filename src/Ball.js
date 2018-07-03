3f5f4d229d712bb59328413b8d3958d467bbb58e
Ralph Gielkens <ralph.gielkens@bct.nl>
2018-07-03 22:12:57 +0200

    Section 2 colage 10, moving the ball from left to right bottom

--- src/Ball.js
+++ src/Ball.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 
 export default class Ball extends Component {
+  componentWillMount(){
+     this.position = new Animated.ValueXY(0,0);
+     Animated.spring(this.position, {
+       toValue: {x:200, y:500}
+     }).start();
+  }
   render() {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+          <View style={styles.ball} />
+      </Animated.View>
     );
   }
 }
@@ -15,9 +23,7 @@ const styles = {
      height:60,
      width:60,
      borderRadius : 30,
-     borderWidth: 30,
+     borderWidth: 30, 
      borderColor: 'red'
    }
 };
-
-// export default Ball;
