d0a7b4e592b855a5632a5b16c8822bac7c3685f6
Ralph Gielkens <ralph.gielkens@bct.nl>
2018-07-03 23:31:55 +0200

    Proberen via een button een animatie uit te voeren en weer te herstellen.

--- src/Ball.js
+++ src/Ball.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, Button } from 'react-native';
 
 
-export default class Ball extends Component {
+export default class Ball extends React.Component {
   componentWillMount(){
      this.position = new Animated.ValueXY(0,0);
-     Animated.spring(this.position, {
-       toValue: {x:200, y:500}
-     }).start();
+     // Animated.spring(this.position, {
+     //   toValue: {x:200, y:500}
+     // }).start();
   }
   render() {
-    return (
+    return ([
       <Animated.View style={this.position.getLayout()}>
           <View style={styles.ball} />
-      </Animated.View>
-    );
+      </Animated.View>,
+      <View style={styles.button} >
+          <Button onPress={
+            () => {
+              Animated.sequence([
+            Animated.spring(this.position, {
+              toValue: {x:200, y:500}
+            }),
+            Animated.spring(this.position, {
+              toValue: {x:0, y:0}
+            })
+            ]).start();
+
+          }} title="Press Me" />
+      </View>
+      ]);
   }
 }
 
 const styles = {
    ball : {
+     position: 'relative',
      height:60,
      width:60,
      borderRadius : 30,
-     borderWidth: 30, 
-     borderColor: 'red'
+     borderWidth: 30,
+     borderColor: 'red',
+      zIndex:10
+   },
+   button: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'center',
    }
 };
